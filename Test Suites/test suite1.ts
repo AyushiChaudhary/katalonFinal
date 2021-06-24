179db837f80ea95c0263270c830cbfbcff443188
Ayushi Chaudhary <ayushichauhary040499@gmail.com>
2021-06-24 14:22:29 +0530

    first commit

---
+++ Test Suites/test suite1.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>test suite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>6c5991c2-9f7c-4c7e-894c-0a0611615320</testSuiteGuid>
+   <testCaseLink>
+      <guid>1d5842b7-8708-44f4-82a8-8d47c1cc3f7b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ts1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
